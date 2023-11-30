6bb984926aa7d70109af826de8f642777b451385
alantiren <tirenalan0@gmail.com>
2023-11-30 19:27:33 +0300

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -7,8 +7,6 @@ import Currency from './3-currency.js';
 export default class Pricing {
   /**
    * Creates a new Pricing instance.
-   * @param {Number} amount - The amount.
-   * @param {Currency} currency - The currency.
    */
   constructor(amount, currency) {
     this._amount = amount;
@@ -17,7 +15,6 @@ export default class Pricing {
 
   /**
    * Getter for the amount.
-   * @returns {Number} - The amount.
    */
   get amount() {
     return this._amount;
@@ -25,9 +22,11 @@ export default class Pricing {
 
   /**
    * Setter for the amount.
-   * @param {Number} value - The new amount.
    */
   set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
     this._amount = value;
   }
 
@@ -44,16 +43,19 @@ export default class Pricing {
    * @param {Currency} value - The new currency.
    */
   set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
     this._currency = value;
   }
+  
 
   /**
    * Displays the full price in the format: amount currency_name (currency_code).
    * @returns {String} - The full price format.
    */
   displayFullPrice() {
-    const currencyDisplay = this._currency.displayFullCurrency();
-    return `${this._amount} ${currencyDisplay}`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   /**
@@ -63,6 +65,12 @@ export default class Pricing {
    * @returns {Number} - The converted amount.
    */
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
     return amount * conversionRate;
   }
 }
