3ae6c00723f2834c5a103c4e8e7a4674e0240dc4
alantiren <tirenalan0@gmail.com>
2023-11-30 19:38:03 +0300

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,28 +2,20 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(name, code) {
-    this._name = name;
-    this._code = code;
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
   }
 
-  get name() {
-    return this._name;
+  get floors() {
+    return this._floors;
   }
 
-  set name(value) {
-    this._name = value;
+  set floors(value) {
+    this._floors = value;
   }
 
-  get code() {
-    return this._code;
-  }
-
-  set code(value) {
-    this._code = value;
-  }
-
-  toString() {
-    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
