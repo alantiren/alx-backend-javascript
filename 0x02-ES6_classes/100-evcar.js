c0db0290fef739296c6c0858eba8b052fc36b5cc
alantiren <tirenalan0@gmail.com>
2023-11-30 19:57:27 +0300

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,7 +3,7 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this.range = range;
   }
 
   get range() {
@@ -15,9 +15,8 @@ export default class EVCar extends Car {
   }
 
   cloneCar() {
-    const Species = this.constructor[Symbol.species];
+    const Species = super.constructor[Symbol.species];
 
-    // Create a new instance of Car with the current attributes
-    return new Car(this.brand, this.motor, this.color);
+    return new Species();
   }
 }
