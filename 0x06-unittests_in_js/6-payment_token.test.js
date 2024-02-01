c73289c1e1f79507438541b49e7196cce9f4e343
alantiren <tirenalan0@gmail.com>
2024-02-01 15:12:22 +0300

    Update 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,21 +2,23 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should return a resolved promise with the expected response when success is true', (done) => {
+  it('should resolve with correct response when success is true', (done) => {
     getPaymentTokenFromAPI(true)
       .then((res) => {
         expect(res).to.deep.equal({ data: 'Successful response from the API' });
         done();
       })
       .catch(done);
+  });
 
-  it('should return a pending promise when success is false', (done) => {
-    const promise = getPaymentTokenFromAPI(false);
-
-    expect(promise).to.be.an.instanceOf(Promise);
-
-    promise
-      .then(() => done(new Error('Promise should not resolve')))
-      .catch(() => done());
+  it('should not resolve when success is false', (done) => {
+    getPaymentTokenFromAPI(false)
+      .then(() => {
+        done(new Error('Promise should not resolve for success=false'));
+      })
+      .catch((err) => {
+        expect(err).to.be.undefined;
+        done();
+      });
   });
 });
